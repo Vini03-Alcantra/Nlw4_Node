705c4d39ca43a416e3855d6e2fbae36667791e85
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-23 17:04:42 -0300

    Criando validação por email

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -7,6 +7,16 @@ class UserController{
         const {name, email} = request.body;
         
         const usersRepository = getRepository(User);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        })
+
+        if(userAlreadyExists){
+            return response.status(400).json({
+                error: "user already exists!"
+            })
+        }
         
         const user = usersRepository.create({
             name,
