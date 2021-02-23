62e80be9c4f57cf33d9aa799347f8832a3e6ef55
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-23 16:02:16 -0300

    Crianto tabela users

--- src/database/migrations/1614104073905-CreateUsers.ts
+++ src/database/migrations/1614104073905-CreateUsers.ts
@@ -3,29 +3,31 @@ import {MigrationInterface, QueryRunner, Table} from "typeorm";
 export class CreateUsers1614104073905 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        new Table({
-            name: "users",
-            columns: [
-                {
-                    name: "id",
-                    type: "uuid",
-                    isPrimary: true
-                },
-                {
-                    name: "name",
-                    type: "varchar"
-                },
-                {
-                    name: "email",
-                    type: "varchar"
-                },
-                {
-                    name: "created_at",
-                    type: "timestamp",
-                    default: "now()"
-                }
-            ]
-        })
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
