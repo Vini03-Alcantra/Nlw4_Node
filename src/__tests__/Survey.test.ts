e184470595d74f773937e653e07f6778f735bd8a
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-25 13:30:52 -0300

    Fazendo buscas no servey com get

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -17,4 +17,14 @@ describe("Surveys", () => {
         expect(response.status).toBe(201)        
         expect(response.body).toHaveProperty("id")
     });
+
+    it("Should be able to get all Surveys", async () => {
+        await request(app).post("/surveys").send({
+            title: "Title Example2",
+            description: "Description Example2",
+        })
+        const response = await request(app).get("/surveys");
+        
+        expect(response.body.length).toBe(2)
+    });
 })
