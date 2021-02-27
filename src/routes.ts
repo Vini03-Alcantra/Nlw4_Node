9772de3cbdcafcc4fba34eeb244d0fe4e99f3c1d
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-27 16:48:50 -0300

    Ccriando um answer controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from "express"
+import { AnswerController } from "./controllers/AnswerController";
 import { SendMailController } from "./controllers/SendMailController";
 import { SurveysController } from "./controllers/SurveysController";
 import {UserController} from "./controllers/UserController";
@@ -9,6 +10,7 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 
 const sendMailController = new SendMailController()
+const answerController = new AnswerController()
 
 router.post("/users", userController.create)
 
@@ -16,6 +18,7 @@ router.post("/surveys", surveysController.create)
 router.get("/surveys", surveysController.show)
 
 router.post("/sendMail", sendMailController.execute)
+router.get("/answers/:value", answerController.execute)
 
 export {router};
 
